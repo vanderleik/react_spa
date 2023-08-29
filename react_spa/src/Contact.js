35c621942fb1ce9f195a7e12d069d85fc416e283
vanderleik <vanderleik@yahoo.com.br>
2023-08-29 17:44:43 -0300

    Create Contact.js

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
